e5dfea41c07c59605ba605cb15b53df3ebe42431
peter@lidee.be <peter@lidee.be>
2021-11-20 18:44:37 +0100

    setting up range

---
+++ components/filters/FilterRange.js
@@ -0,0 +1,84 @@
+import { Range, getTrackBackground } from 'react-range';
+
+function FilterRange(props){
+  const sliderMin = props.min;
+  const sliderMax = props.max;
+  const values = props.sliderSelection;
+  return(
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        flexWrap: 'wrap'
+      }}
+    >
+      <Range
+        values={values}
+        step={props.steps}
+        min={sliderMin}
+        max={sliderMax}
+        onChange={(values) => props.handleSliderSelection(values)}
+        onFinalChange={(values) => props.handleSliderFinalSelection(values)}
+
+        renderTrack={({ props, children }) => (
+          <div
+            onMouseDown={props.onMouseDown}
+            onTouchStart={props.onTouchStart}
+            style={{
+              ...props.style,
+              height: '36px',
+              display: 'flex',
+              width: '100%'
+            }}
+          >
+            <div
+              ref={props.ref}
+              style={{
+                height: '5px',
+                width: '100%',
+                borderRadius: '4px',
+                background: getTrackBackground({
+                  values,
+                  colors: ['#ccc', '#548BF4', '#ccc'],
+                  min: sliderMin,
+                  max: sliderMax,
+                }),
+                alignSelf: 'center'
+              }}
+            >
+              {children}
+            </div>
+          </div>
+        )}
+        renderThumb={({ props, isDragged }) => (
+          <div
+            {...props}
+            style={{
+              ...props.style,
+              height: '42px',
+              width: '42px',
+              borderRadius: '4px',
+              backgroundColor: '#FFF',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              boxShadow: '0px 2px 6px #AAA'
+            }}
+          >
+            <div
+              style={{
+                height: '16px',
+                width: '5px',
+                backgroundColor: isDragged ? '#548BF4' : '#CCC'
+              }}
+            />
+          </div>
+        )}
+
+
+      />
+    </div>
+  )
+}
+
+export default FilterRange;
--- components/filters/NumberFilter.js
+++ components/filters/NumberFilter.js
@@ -1,9 +1,23 @@
 import { useState } from "react";
+import FilterRange from "./FilterRange";
+
+
 
 function NumberFilter(props){
 
   const [ min, setMin ] = useState(0)
   const [ max, setMax ] = useState(9500)
+
+  const [ sliderMin, setSliderMin ] = useState(0);
+  const [ sliderMax, setSliderMax ] = useState(100);
+
+  // const [values, setValues] = useState([30, 70]);
+  // const [finalValues, setFinalValues] = useState([30, 70]);
+
+  const [inputSelection, setInputSelection] = useState([0,100])
+
+  const [sliderSelection, setSliderSelection] = useState([0,100])
+  const [sliderFinalSelection, setSliderFinalSelection] = useState([0,100])
 
   const handleInputChange = (e, type) => {
     const value = +e.target.value;
@@ -15,15 +29,54 @@ function NumberFilter(props){
     }
   }
 
+  const handleInputSelection = (val) => {
+    setInputSelection(val)
+  }
+
+  // const onSliderChange = () => {
+  //   console.log('onSliderChange',)
+  // }
+
+  const handleSliderSelection = (val) => {
+    setInputSelection(val);
+    setSliderSelection(val);
+  }
+  const handleSliderFinalSelection = (val) => {
+    setSliderFinalSelection(val);
+  }
+
   return(
     <div>
       numberfilter {props.filter.name}
-      slider
+      <FilterRange 
+        min={0} 
+        max={100} 
+        steps={1}
+        sliderSelection={sliderSelection} 
+        setSliderSelection={setSliderSelection} 
+        sliderFinalSelection={sliderFinalSelection} 
+        setSliderFinalSelection={setSliderFinalSelection}
+        handleSliderSelection={handleSliderSelection}
+        handleSliderFinalSelection={handleSliderFinalSelection}
+      />
       <div>
+        sliderSelection: {sliderSelection[0]} - {sliderSelection[1]}
+      </div>
+      <div>
+        sliderFinalSelection: {sliderFinalSelection[0]} - {sliderFinalSelection[1]}
+      </div>
+      {/*<div>
         <label htmlFor={`numberfilter-${props.filter}-from`}>from</label>
         <input type="number" value={min} id={`numberfilter-${props.filter}-from`} onChange={(e) => handleInputChange(e, "min")} />
         <label htmlFor={`numberfilter-${props.filter}-to`}>to</label>
         <input type="number" value={max} id={`numberfilter-${props.filter}-to`} onChange={(e) => handleInputChange(e, "max")} />
+        <button>filter</button>
+      </div>*/}
+      <div>
+        <label htmlFor={`numberfilter-${props.filter}-from`}>from</label>
+        <input type="number" value={inputSelection[0]} id={`numberfilter-${props.filter}-from`} onChange={(e) => handleInputSelection([e.target.value, inputSelection[1]])} />
+        <label htmlFor={`numberfilter-${props.filter}-to`}>to</label>
+        <input type="number" value={inputSelection[1]} id={`numberfilter-${props.filter}-to`} onChange={(e) => handleInputSelection([inputSelection[0], e.target.value])} />
         <button>filter</button>
       </div>
     </div>
