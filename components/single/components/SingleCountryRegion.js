7d7ff33558c5eee1cd19ff3bee72f8de0c8b9545
peter@lidee.be <peter@lidee.be>
2021-12-10 15:13:10 +0100

    Added capital and conditional rendering

--- components/single/components/SingleCountryRegion.js
+++ components/single/components/SingleCountryRegion.js
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import Proptypes from 'prop-types';
 import Wrapper from '../../general/Wrapper';
 import NeighbouringCountries from './NeighbouringCountries';
@@ -8,10 +7,24 @@ function SingleCountryRegion(props){
 
   return(
     <Wrapper base="single-country__component" modifier="region">
-      <div className="single-country__label">region</div>
-      <div className="single-country__value">{props.data.region}</div>
-      <div className="single-country__label">subregion</div>
-      <div className="single-country__value">{props.data.subregion}</div>
+      {props.data.region && 
+        <>
+          <div className="single-country__label">region</div>
+          <div className="single-country__value">{props.data.region}</div>
+        </>
+      }
+      {props.data.subregion && 
+        <>
+          <div className="single-country__label">subregion</div>
+          <div className="single-country__value">{props.data.subregion}</div>
+        </>
+      }
+      {props.data.capital && 
+        <>
+          <div className="single-country__label">capital</div>
+          <div className="single-country__value">{props.data.capital}</div>
+        </>
+      }
       <NeighbouringCountries borders={props.data.borders}>
         {props.children}
       </NeighbouringCountries>
