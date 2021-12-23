91d59b9b64fccff7f95d7a89c37c5cc3b3ecb1dd
peter@lidee.be <peter@lidee.be>
2021-12-23 15:28:44 +0100

    Added backgroundColor prop to placeholder

--- components/svgSnippets/Placeholder.js
+++ components/svgSnippets/Placeholder.js
@@ -1,8 +1,11 @@
-export default function Placeholder(props){
-  const svgClass = props.place ? `svg-placeholder svg-placeholder--${props.place}` : "svg-placeholder";
-  return(
-    <svg xmlns="http://www.w3.org/2000/svg" width="30px" height="20px" viewBox="0 0 30 20" className={`svg-placeholder ${props.extraClass && props.extraClass}`}>
-      <rect x="0" y="0" width="30" height="20" fill="#fff" />
-    </svg>
-  )
+const Placeholder = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="30px" height="20px" viewBox="0 0 30 20" className={`svg-placeholder ${props.extraClass && props.extraClass}`}>
+    <rect x="0" y="0" width="30" height="20" fill={props.backgroundColor} />
+  </svg>
+)
+
+Placeholder.defaultProps = {
+  backgroundColor: "#fff",
 }
+
+export default Placeholder;
