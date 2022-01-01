f50a9c3d12b5a5bb9b91927152878d0663946eb6
peter@lidee.be <peter@lidee.be>
2022-01-01 16:12:29 +0100

    removed box from title

--- components/single/sections/SingleCountryHeader.js
+++ components/single/sections/SingleCountryHeader.js
@@ -2,9 +2,7 @@ import PropTypes from 'prop-types';
 
 const SingleCountryTitle = (props) => (
   <div className="single-country__header">
-    <div className="single-country__box">
-      <h1 className="single-country__title">{props.countryName}</h1>
-    </div>
+    <h1 className="single-country__title">{props.countryName}</h1>
     {props.children}
   </div>
 )
