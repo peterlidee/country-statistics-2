046625377772d989d417f3865dde5b7382a9406c
peter@lidee.be <peter@lidee.be>
2022-01-05 12:10:33 +0100

    Removed preloading for gfonts as per next docs

--- pages/_document.js
+++ pages/_document.js
@@ -7,8 +7,6 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Inconsolata&family=Quicksand:wght@400;500;700&display=swap" rel="stylesheet" />
       </Head>
       <body>
