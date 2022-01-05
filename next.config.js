815e96bd1902c3fcf389ffd2fd02ca57e4dee8cb
peter@lidee.be <peter@lidee.be>
2022-01-05 15:32:36 +0100

    switched from env to .env.local and removed dotenv for next inbuild env support

--- next.config.js
+++ next.config.js
@@ -1,10 +1,3 @@
-const webpack = require('webpack')
-const { parsed: localEnv } = require('dotenv').config()
-
 module.exports = {
   reactStrictMode: true,
-  webpack(config) {
-    config.plugins.push(new webpack.EnvironmentPlugin(localEnv))
-    return config
-  }
 }
