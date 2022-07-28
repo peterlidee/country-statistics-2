8602bf8530f147320a17f53b491368c67570ead1
peterlidee <peter@lidee.be>
2022-07-28 18:20:33 +0200

    Added test for home page

---
+++ __test__/index.test.js
@@ -0,0 +1,23 @@
+import { screen, render } from '@testing-library/react'
+import { toBeInTheDocument } from '@testing-library/jest-dom'
+import HomePage from "../pages"
+import Home from '../components/Home'
+
+jest.mock('../components/Home', () => {
+  return jest.fn((props) => <div data-testid="Home" {...props} />)
+})
+
+test('HomePage renders because Home was called', () => {
+  render(<HomePage />)
+  expect(screen.getByTestId('Home')).toBeInTheDocument()
+})
+
+test('Home mock is called with the correct props', () => {
+  render(<HomePage countries={[1,2,3]} endpoint="url" />)
+  screen.debug()
+  expect(Home).toHaveBeenCalledWith(
+    expect.objectContaining({
+    endpoint: 'url',
+    countries: [1,2,3],
+  }), expect.anything())
+})
