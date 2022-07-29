6292e4ea648143e1e09adbac70a2c25a5b84a87a
peterlidee <peter@lidee.be>
2022-07-29 17:09:35 +0200

    restructure test

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -7,16 +7,17 @@ jest.mock('../components/Home', () => {
   return jest.fn((props) => <div data-testid="Home" {...props} />)
 })
 
-test('HomePage renders because Home was called', () => {
-  render(<HomePage />)
-  expect(screen.getByTestId('Home')).toBeInTheDocument()
-})
-
-test('Home mock is called with the correct props', () => {
-  render(<HomePage countries={[1,2,3]} endpoint="url" />)
-  expect(Home).toHaveBeenCalledWith(
-    expect.objectContaining({
-    endpoint: 'url',
-    countries: [1,2,3],
-  }), expect.anything())
+describe('page/index.js', () => {
+  test('It renders because Home was called', () => {
+    render(<HomePage />)
+    expect(screen.getByTestId('Home')).toBeInTheDocument()
+  })
+  test('Home mock is called with the correct props', () => {
+    render(<HomePage countries={[1,2,3]} endpoint="url" />)
+    expect(Home).toHaveBeenCalledWith(
+      expect.objectContaining({
+      endpoint: 'url',
+      countries: [1,2,3],
+    }), expect.anything())
+  })
 })
