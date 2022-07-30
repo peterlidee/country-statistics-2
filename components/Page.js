98f83ea15ee66cc83a903f007dcdadf9cb6bf541
peterlidee <peter@lidee.be>
2022-07-30 14:35:33 +0200

    Fixed tabs

--- components/Page.js
+++ components/Page.js
@@ -2,18 +2,18 @@ import Head from 'next/head';
 import Footer from "./Footer";
 
 function Page(props){
-    return(
-        <>
-          <Head>
-            <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport" />
-            <link rel="icon" href="/favicon.png" />
-          </Head>
-          <div className="site__container">
-            {props.children}
-          </div>
-          <Footer />
-        </>
-    )
+  return(
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <div className="site__container">
+        {props.children}
+      </div>
+      <Footer />
+    </>
+  )
 }
 
 export default Page;
