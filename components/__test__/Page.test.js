e2aff4183aab09b1bd263f782aa9c28a25aaac31
peterlidee <peter@lidee.be>
2022-07-30 14:51:13 +0200

    added test for components/Page

---
+++ components/__test__/Page.test.js
@@ -0,0 +1,21 @@
+import { render, screen, toHaveBeenCalled } from '@testing-library/react'
+import { toBeInTheDocument } from '@testing-library/jest-dom'
+import Page from "../Page"
+import Footer from "../Footer"
+
+jest.mock('../Footer', () => {
+  return jest.fn(() => <div data-testid="Footer" />)
+})
+
+describe('components/Page', () => {
+  test('It renders', () => {
+    const { container } = render(<Page />)
+    screen.debug()
+    expect(container.querySelector('.site__container')).toBeInTheDocument()
+    expect(Footer).toHaveBeenCalled()
+  })
+  test('It passes props.children', () => {
+    const { container } = render(<Page><div data-testid="Child" /></Page>)
+    expect(screen.getByTestId('Child')).toBeInTheDocument()
+  })
+})
