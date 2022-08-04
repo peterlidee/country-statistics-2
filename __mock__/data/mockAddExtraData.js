761bb9c4188e5136024d21935f817009f96adedb
peterlidee <peter@lidee.be>
2022-08-04 17:39:01 +0200

    Changed formatting

--- __mock__/data/mockAddExtraData.js
+++ __mock__/data/mockAddExtraData.js
@@ -5,5 +5,10 @@
 // [2] = broken example no population
 // [3] = example with name Åland
 export default function mockAddExtraData(){
-  return [{"name":{"common":"Test"},"area":10000,"population":10000000},{"name":{"common":"Test"},"population":10000000},{"name":{"common":"Test"},"area":10000},{"name":{"common":"Åland Islands"},"cca3":"ALA"}]
+  return [
+    {"name":{"common":"Test"},"area":10000,"population":10000000},
+    {"name":{"common":"Test"},"population":10000000},
+    {"name":{"common":"Test"},"area":10000},
+    {"name":{"common":"Åland Islands"},"cca3":"ALA"}
+  ]
 }
