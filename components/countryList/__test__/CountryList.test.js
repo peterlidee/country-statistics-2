e939ef97ba676e7de193d9574379c7aed4f93041
peterlidee <peter@lidee.be>
2022-08-06 15:27:07 +0200

    Added test for CountryList

---
+++ components/countryList/__test__/CountryList.test.js
@@ -0,0 +1,50 @@
+import { screen, render } from '@testing-library/react'
+import {toBeInTheDocument } from '@testing-library/jest-dom'
+
+import { FieldsContextProvider } from '../../context/FieldsContext'
+import { NumberFiltersContextProvider } from '../../context/NumberFiltersContext'
+import { RegionFilterContextProvider } from '../../context/RegionFilterContext'
+
+import CountryList from '../CountryList'
+import CountryCount from '../../header/CountryCount'
+import Filters from '../../filters/Filters'
+import CountryListHeaders from '../CountryListHeaders'
+import CountryRow from '../CountryRow'
+
+import countriesMock from '../../../__mock__/data/countriesMock'
+import filterDataMock from '../../../__mock__/data/filterDataMock'
+
+const { extraDataCountries } = countriesMock
+
+jest.mock('../../header/CountryCount', () => {
+  return jest.fn(() => <div data-testid="CountryCount" />)
+})
+jest.mock('../../filters/Filters')
+jest.mock('../CountryListHeaders')
+jest.mock('../CountryRow')
+
+describe('components/countryList/CountryList', () => {
+  test('It renders', () => {
+    const { container } = render(
+      <FieldsContextProvider>
+        <RegionFilterContextProvider defaultRegionState={filterDataMock.defaultRegionState}>
+          <NumberFiltersContextProvider filterData={filterDataMock}>
+            <CountryList countries={extraDataCountries} filterData={filterDataMock} />
+          </NumberFiltersContextProvider>
+        </RegionFilterContextProvider>
+      </FieldsContextProvider>
+    )
+
+    expect(container.querySelector('.site__grid--home')).toBeInTheDocument()
+    expect(screen.getByTestId('CountryCount')).toBeInTheDocument()
+    expect(CountryCount).toHaveBeenCalledWith(
+      { count: 6 },
+      expect.anything()
+    )
+    expect(Filters).toHaveBeenCalled()
+    expect(CountryListHeaders).toHaveBeenCalled()
+    expect(CountryRow).toHaveBeenCalledTimes(6)
+    expect(CountryRow.mock.calls[0][0].country.cca3).toBe('AUT')
+    expect(CountryRow.mock.calls[5][0].country.cca3).toBe('SGS')
+  })
+})
