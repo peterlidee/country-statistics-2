27355df30e339335fb47c2c247cdc3e213281ef7
peterlidee <peter@lidee.be>
2022-08-07 11:34:53 +0200

    Added test for CountryListHeader

---
+++ components/countryList/__test__/CountryListHeader.test.js
@@ -0,0 +1,36 @@
+import { screen, render } from '@testing-library/react'
+import { toBeInTheDocument } from '@testing-library/jest-dom'
+
+import CountryListHeader from '../CountryListHeader'
+import Wrapper from '../../general/Wrapper'
+import IconSort from '../../svgSnippets/IconSort'
+// import FieldsContext
+
+jest.mock('../../general/Wrapper', () => {
+  return jest.fn((props) => <div data-testid="Wrapper">{props.children}</div>)
+})
+jest.mock('../../svgSnippets/IconSort')
+
+describe('components/countryList/CountryListHeader', () => {
+  test('It renders', () => {
+    render(<CountryListHeader field={{ 
+      display: true,
+      field: 'test',
+      label: 'label',
+      sortActive: true,
+      sortAsc: true,
+    }} handleSort={() => {}} />)
+    expect(Wrapper).toHaveBeenCalled()
+    expect(IconSort).toHaveBeenCalledWith(
+      expect.objectContaining({
+        sortActive: true,
+        sortAsc: true,
+      }),
+      expect.anything()
+    )
+    const button = screen.getByRole('button')
+    expect(button).toBeInTheDocument()
+    expect(button).toHaveTextContent('label')
+    expect(button).toHaveClass('button__sort--test')
+  })
+})
