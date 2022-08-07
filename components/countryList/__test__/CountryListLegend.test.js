76672f3a3115f13cc0d85bd3d272f5ba970ebf52
peterlidee <peter@lidee.be>
2022-08-07 11:49:12 +0200

    Added test for CountryListLegend

---
+++ components/countryList/__test__/CountryListLegend.test.js
@@ -0,0 +1,30 @@
+import { screen, render } from '@testing-library/react'
+import { toBeInTheDocument } from '@testing-library/jest-dom'
+
+import CountryListLegend from '../CountryListLegend'
+import Wrapper from '../../general/Wrapper'
+
+jest.mock('../../general/Wrapper', () => {
+  return jest.fn((props) => <div data-testid="Wrapper">{props.children}</div>)
+})
+
+describe('components/countrylist/CountryListLegend.js', () => {
+  test('It does not render when no field.display data', () => {
+    render(<CountryListLegend field={{
+      display: false,
+      field: 'field',
+      legend: 'legend',
+    }} />)
+    expect(Wrapper).not.toHaveBeenCalled()
+    expect(screen.queryByText('legend')).not.toBeInTheDocument()
+  })
+  test('It renders', () => {
+    render(<CountryListLegend field={{
+      display: true,
+      field: 'field',
+      legend: 'legend',
+    }} />)
+    expect(Wrapper).toHaveBeenCalled()
+    expect(screen.getByText('legend')).toBeInTheDocument()
+  })
+})
