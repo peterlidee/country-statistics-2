4f21ea22680a58333f4a4fd4c41c4bbfed68b612
peterlidee <peter@lidee.be>
2022-08-08 20:51:45 +0200

    Added test for FilterRow

---
+++ components/filters/__test__/region/FilterRow.test.js
@@ -0,0 +1,33 @@
+import { screen, render } from '@testing-library/react'
+import { toBeInTheDocument } from '@testing-library/jest-dom'
+
+import FilterRow from '../../region/FilterRow'
+import FilterCheckBox from '../../region/FilterCheckbox'
+
+jest.mock('../../region/FilterCheckbox')
+
+describe('components/filters/region/FilterRow', () => {
+  test('It renders', () => {
+    const { container } = render(
+      <FilterRow 
+        name="name"
+        active={true} 
+        handler={() => {}} 
+        count={1}>
+        <div data-testid="ChildMock" />
+      </FilterRow>
+    )
+    screen.debug()
+    expect(container.querySelector('.filter__row')).toBeInTheDocument()
+    expect(FilterCheckBox).toHaveBeenCalledWith(
+      expect.objectContaining({
+        label: 'name', active: true,
+      }),
+      expect.anything()
+    )
+    expect(container.querySelector('.filtercheckbox__count')).toBeInTheDocument()
+    expect(container.querySelector('.filtercheckbox__count-inner')).toBeInTheDocument()
+    expect(container.querySelector('.filtercheckbox__count-inner')).toHaveTextContent('1')
+    expect(screen.getByTestId('ChildMock')).toBeInTheDocument()
+  })
+})
