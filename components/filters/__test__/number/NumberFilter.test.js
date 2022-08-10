88e47b480933355185185b2786a7e785c5f69ded
peterlidee <peter@lidee.be>
2022-08-10 20:39:36 +0200

    Added an extra test for NumberFilter

--- components/filters/__test__/number/NumberFilter.test.js
+++ components/filters/__test__/number/NumberFilter.test.js
@@ -1,5 +1,6 @@
 import { screen, render } from '@testing-library/react'
 import { toBeInTheDocument } from '@testing-library/jest-dom'
+import userEvent from '@testing-library/user-event'
 
 import filterDataMock from '../../../../__mock__/data/filterDataMock'
 import {FieldsContextProvider } from '../../../context/FieldsContext'
@@ -38,5 +39,76 @@ describe('components/filters/number/NumberFilter', () => {
     expect(screen.getByRole('spinbutton', { name: 'to' })).toBeInTheDocument()
     expect(screen.getByRole('button', { name: '>' })).toBeInTheDocument()
     expect(screen.getByRole('button', { name: 'clear' })).toBeInTheDocument()
+  })
+
+  test('Controls work', async () => {
+    render(
+      <FieldsContextProvider>
+        <NumberFiltersContextProvider filterData={filterDataMock}>
+          <NumberFilter 
+            filter={'density'} 
+            currFilterData={filterDataMock['density']} />
+        </NumberFiltersContextProvider>
+      </FieldsContextProvider>
+    )
+    const User = userEvent.setup()
+    const inputFrom = screen.getByRole('spinbutton', { name: 'from' })
+    const inputTo = screen.getByRole('spinbutton', { name: 'to' })
+    const submitButton = screen.getByRole('button', { name: '>' })
+    const clearButton = screen.getByRole('button', { name: 'clear' })
+
+    // enter a string (number) in from field
+    await User.type(inputFrom, '100')
+    expect(inputFrom).toHaveValue(100)
+    
+    // another number
+    await User.clear(inputFrom)
+    await User.type(inputFrom, '200')
+    expect(inputFrom).toHaveValue(200)
+    
+    // fails to set negative number
+    await User.clear(inputFrom)
+    await User.type(inputFrom, '-200')
+    expect(inputFrom).not.toHaveValue(-200)
+    expect(inputFrom).toHaveValue(200)
+
+    // on submit, 
+    // the values should remain
+    await User.clear(inputFrom)
+    await User.type(inputFrom, '100')
+    await User.clear(inputTo)
+    await User.type(inputTo, '300')
+    await User.click(submitButton)
+    expect(inputFrom).toHaveValue(100)
+    expect(inputTo).toHaveValue(300)
+
+    // on submit, 
+    // if TO is smaller then FROM, then the values will be flipped
+    await User.clear(inputFrom)
+    await User.type(inputFrom, '300')
+    await User.clear(inputTo)
+    await User.type(inputTo, '100')
+    await User.click(submitButton)
+    expect(inputFrom).toHaveValue(100)
+    expect(inputTo).toHaveValue(300)
+
+    // on submit, 
+    // is TO is higher then max, it gets corrected to max
+    await User.clear(inputTo)
+    await User.type(inputTo, '500')
+    await User.click(submitButton)
+    expect(inputFrom).toHaveValue(100)
+    expect(inputTo).toHaveValue(400)
+
+    // FilterInput was called each time with updated values
+    // we check the last call
+    const filterRangeCalls = FilterRange.mock.calls
+    expect(filterRangeCalls[filterRangeCalls.length - 1][0].sliderSelection[0]).toBe(100)
+    expect(filterRangeCalls[filterRangeCalls.length - 1][0].sliderSelection[1]).toBe(400)
+
+    // check if clearbutton works
+    await User.click(clearButton)
+    expect(inputFrom).toHaveValue(0)
+    expect(inputTo).toHaveValue(400)
   })
 })
