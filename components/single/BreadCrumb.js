e0c1823190951e9c52e6e6ca1c35d81b229a4d22
peterlidee <peter@lidee.be>
2022-08-11 17:12:57 +0200

    Fixed typo

--- components/single/BreadCrumb.js
+++ components/single/BreadCrumb.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 const BreadCrumb = (props) => (
   <div className="breadcrumb">
     <Link href="/">
-      <a className="breabcrumb__home">all countries</a>
+      <a className="breadcrumb__home">all countries</a>
     </Link>
     <span className="breadcrumb__divider">&gt;</span>
     {props.countryName}
