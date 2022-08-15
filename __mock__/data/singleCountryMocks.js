d4696a449efa2a06b178f8d5afd4ae4a8a920611
peterlidee <peter@lidee.be>
2022-08-15 11:32:58 +0200

    Added country to singleCountryMocks

--- __mock__/data/singleCountryMocks.js
+++ __mock__/data/singleCountryMocks.js
@@ -23,7 +23,31 @@ const singleCountryMocks = [
       svg: "https://mainfacts.com/media/images/coats_of_arms/dz.svg"
     },
   },
-
+  // same country without subregion
+  {
+    "name": {
+      common: "Algeria",
+    },
+    "cca2": "DZ",
+    "ccn3": "012",
+    "cca3": "DZA",
+    "cioc": "ALG",
+    capital: ["Algiers"],
+    region: "Africa",
+    // subregion: "Northern Africa",
+    latlng: [28, 3],
+    borders: ["TUN","LBY","NER","ESH","MRT","MLI","MAR"],
+    area: 2381741,
+    population: 44700000,
+    flags: {
+      png: "https://flagcdn.com/w320/dz.png",
+      svg: "https://flagcdn.com/dz.svg"
+    },
+    coatOfArms: {
+      png: "https://mainfacts.com/media/images/coats_of_arms/dz.png",
+      svg: "https://mainfacts.com/media/images/coats_of_arms/dz.svg"
+    },
+  },
 ]
 
 export default singleCountryMocks
