ed3707555ea1a54ca1f50fce5a0e17145e8323da
peterlidee <peter@lidee.be>
2022-08-16 11:55:21 +0200

    Add test for components/single/chart/PopulationChartWidget

---
+++ components/single/chart/__test__/PopulationChartWidget.test.js
@@ -0,0 +1,36 @@
+import { screen, render } from '@testing-library/react'
+import { toBeInTheDocument } from '@testing-library/jest-dom'
+
+import PopulationChartWidget from '../PopulationChartWidget'
+import chartDataMock from '../../../../__mock__/data/chartDataMock'
+import { Chart } from 'react-chartjs-2'
+
+jest.mock('react-chartjs-2', () => {
+  return({
+    Chart: jest.fn()
+  })
+})
+
+describe('components/single/chart/PopulationChartWidget', () => {
+  test('It renders', () => {
+    const { container } = render(
+      <PopulationChartWidget {...chartDataMock} />
+    )
+    expect(screen.getByRole('heading', { level: 4 })).toHaveTextContent('Population evolution 2016-2020')
+    expect(container.querySelector('.chart-container')).toBeInTheDocument()
+    expect(Chart).toHaveBeenCalledWith(
+      expect.objectContaining({
+        type: 'bar',
+        // we check only one prop
+        data: expect.objectContaining({
+          labels: [2016,2017,2018,2019,2020]
+        }),
+        // we check only one prop
+        options: expect.objectContaining({
+          responsive: true
+        }),
+      }),
+      expect.anything()
+    )
+  })
+})
