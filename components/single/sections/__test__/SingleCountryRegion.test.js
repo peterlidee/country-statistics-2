6d5ef71dfca9fdde3cf5e31ffe860388e9b349a5
peterlidee <peter@lidee.be>
2022-08-16 16:57:59 +0200

    Added rerender test to SingleCountryRegion

--- components/single/sections/__test__/SingleCountryRegion.test.js
+++ components/single/sections/__test__/SingleCountryRegion.test.js
@@ -6,9 +6,7 @@ import SingleCountryRegion from '../SingleCountryRegion'
 import RenderLabelValue from '../../region/RenderLabelValue'
 import ValidateNeighbouringCountries from '../../neighbours/ValidateNeighbouringCountries'
 
-jest.mock('../../region/RenderLabelValue', () => {
-  return jest.fn(props => <div className="RenderLabelValue">{props.children}</div>)
-})
+jest.mock('../../region/RenderLabelValue')
 jest.mock('../../neighbours/ValidateNeighbouringCountries')
 
 describe('components/single/sections/SingleCountryRegion', () => {
@@ -21,6 +19,7 @@ describe('components/single/sections/SingleCountryRegion', () => {
         data={singleCountryMocks[0]}
       />
     )
+    screen.debug()
     expect(container.querySelector('.single-country__region')).toBeInTheDocument()
     expect(container.querySelector('.single-country__box')).toBeInTheDocument()
     expect(RenderLabelValue.mock.calls).toHaveLength(3)
@@ -28,7 +27,7 @@ describe('components/single/sections/SingleCountryRegion', () => {
       loading: false,
       hasData: true,
       value: 'Africa',
-      label: 'region',
+      // label: 'region',
     })
     expect(RenderLabelValue.mock.calls[1][0]).toMatchObject({
       loading: false,
@@ -77,6 +76,31 @@ describe('components/single/sections/SingleCountryRegion', () => {
     expect(RenderLabelValue.mock.calls[0][0]).toHaveProperty('value', undefined)
     expect(RenderLabelValue.mock.calls[1][0]).toHaveProperty('value', undefined)
     expect(RenderLabelValue.mock.calls[2][0]).toHaveProperty('value', null)
+  })
+
+  test('It rerenders correctly', () => {
+    jest.clearAllMocks()
+    const { container, rerender } = render(
+      <SingleCountryRegion 
+        loading={false}
+        error={undefined}
+        data={singleCountryMocks[0]}
+      />
+    )
+    expect(RenderLabelValue.mock.calls[0][0]).toMatchObject({ value: 'Africa' })
+    expect(RenderLabelValue.mock.calls[1][0]).toMatchObject({ value: 'Northern Africa' })
+    expect(RenderLabelValue.mock.calls[2][0]).toMatchObject({ value: 'Algiers' })
+    
+    rerender(
+      <SingleCountryRegion 
+        loading={false}
+        error={undefined}
+        data={singleCountryMocks[3]}
+      />
+    )
+    expect(RenderLabelValue.mock.calls[3][0]).toMatchObject({ value: 'Europe' })
+    expect(RenderLabelValue.mock.calls[4][0]).toMatchObject({ value: 'Western Europe' })
+    expect(RenderLabelValue.mock.calls[5][0]).toMatchObject({ value: 'Brussels' })
   })
 
 })
