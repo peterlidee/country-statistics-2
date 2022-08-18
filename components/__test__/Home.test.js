8602e0259794c57150ec61f0a8b4bf6e76e89402
peterlidee <peter@lidee.be>
2022-08-18 17:57:26 +0200

    Updated test to use automatic mocking

--- components/__test__/Home.test.js
+++ components/__test__/Home.test.js
@@ -7,37 +7,27 @@ import CountryList from '../countryList/CountryList'
 import Sources from '../sources/Sources'
 import Source from '../sources/Source'
 
-jest.mock('../header/Header', () => {
-  return jest.fn(() => <div data-testid="Header" />)
-})
-jest.mock('../countryList/CountryList', () => {
-  return jest.fn(() => <div data-testid="CountryList" />)
-})
+jest.mock('../header/Header')
+jest.mock('../countryList/CountryList')
 jest.mock('../sources/Sources', () => {
   return jest.fn((props) => <div data-testid="Sources">{props.children}</div>)
 })
-jest.mock('../sources/Source', () => {
-  return jest.fn(() => <div data-testid="Source" />)
-})
+jest.mock('../sources/Source')
 
 describe('components/Home', () => {
   test('It renders', () => {
     render(<Home endpoint="endpoint" countries={[]} />)
 
-    expect(screen.getByTestId('Header')).toBeInTheDocument()
     expect(Header).toHaveBeenCalledWith(
       { 'home': true }, 
       expect.anything()
     )
-    expect(screen.getByTestId('CountryList')).toBeInTheDocument()
     expect(CountryList).toHaveBeenCalledWith(
       expect.objectContaining({ 'countries': [] }), 
       expect.anything()
     )
     expect(screen.getByTestId('Sources')).toBeInTheDocument()
     expect(Sources).toHaveBeenCalled()
-    expect(screen.getByTestId('Source')).toBeInTheDocument()
-    expect(Source).toHaveBeenCalled()
     expect(Source).toHaveBeenCalledWith({
       error: false,
       loading: false,
