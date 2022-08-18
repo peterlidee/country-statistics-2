720a90760fcbf84d8ba36b32647db40d46f8b7e2
peterlidee <peter@lidee.be>
2022-08-18 18:44:23 +0200

    Updated with ChildMock jest.fn

--- components/sources/__test__/Sources.test.js
+++ components/sources/__test__/Sources.test.js
@@ -2,14 +2,15 @@ import { screen, render } from '@testing-library/react'
 import { toBeInTheDocument } from '@testing-library/jest-dom'
 
 import Sources from '../Sources'
+const ChildMock = jest.fn()
 
 describe('components/sources/Sources', () => {
   test('It renders', () => {
-    const { container } = render(<Sources><div data-testid="kids" /></Sources>)
+    const { container } = render(<Sources><ChildMock /></Sources>)
     expect(container.querySelector('div')).toBeInTheDocument()
   })
   test('It renders the kids', () => {
-    const { container } = render(<Sources><div data-testid="kids" /></Sources>)
-    expect(screen.getByTestId('kids')).toBeInTheDocument()
+    render(<Sources><ChildMock /></Sources>)
+    expect(ChildMock).toHaveBeenCalled()
   })
 })
