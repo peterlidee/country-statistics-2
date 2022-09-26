a8d3b388d61f8b218b065f1ef5de55c9d9dabf94
peterlidee <peter@lidee.be>
2022-09-26 15:29:56 +0200

    Added logic for hide and sortAsc and sortBy props

--- components/countryList/CountryListHeaders.js
+++ components/countryList/CountryListHeaders.js
@@ -1,22 +1,36 @@
 // displays the header row for CountryList
 
+import PropTypes from 'prop-types';
+
 import fieldsData from '../fields/fieldsData'
 import Wrapper from '../general/Wrapper'
 import CountryListHeader from './CountryListHeader'
 import CountryListLegend from './CountryListLegend'
 
 // all header fields for countries list
-function CountryListHeaders(){
+function CountryListHeaders({ hiddenFields, sortBy, sortAsc }){
   return(
     <>
       <Wrapper base={'country-list-header'} modifier={'index'}>{null}</Wrapper>
-      {fieldsData.map((field, i) => 
-        <CountryListHeader key={field.slug} field={field} />)}
+      {fieldsData.map((field, i) => {
+        // check if the field is to be displayed or not
+        if(hiddenFields.includes(field.slug)) return null
+        return <CountryListHeader key={field.slug} field={field} sortBy={sortBy} sortAsc={sortAsc} />
+      })}
       <Wrapper base={'country-list-legend'} modifier={'index'}>{null}</Wrapper>
-      {fieldsData.map((field, i) => 
-        <CountryListLegend field={field} key={`country-list-legend-${field.slug}`} />)}
+      {fieldsData.map((field, i) => {
+        // check if the field legend is to be displayed or not
+        if(hiddenFields.includes(field.slug)) return null
+        return <CountryListLegend field={field} key={`country-list-legend-${field.slug}`} />
+      })}
     </>
   )
 }
 
-export default CountryListHeaders;
+CountryListHeaders.propTypes = {
+  hiddenFields: PropTypes.array.isRequired,
+  sortBy: PropTypes.string.isRequired,
+  sortAsc: PropTypes.bool.isRequired,
+}
+
+export default CountryListHeaders
