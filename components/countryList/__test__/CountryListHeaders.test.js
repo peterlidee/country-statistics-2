e1741bc0645f5ad6f30d1d848745e0e09377f58b
peterlidee <peter@lidee.be>
2022-09-26 17:56:17 +0200

    Updated test for changes made

--- components/countryList/__test__/CountryListHeaders.test.js
+++ components/countryList/__test__/CountryListHeaders.test.js
@@ -1,6 +1,5 @@
-import { screen, render } from '@testing-library/react'
+import { render } from '@testing-library/react'
 
-import { FieldsContextProvider } from '../../context/FieldsContext'
 import CountryListHeaders from '../CountryListHeaders'
 import CountryListHeader from '../CountryListHeader'
 import CountryListLegend from '../CountryListLegend'
@@ -9,18 +8,49 @@ import Wrapper from '../../general/Wrapper'
 jest.mock('../../general/Wrapper', () => {
   return jest.fn((props) => <>{props.children}</>)
 })
-jest.mock('../CountryListLegend')
 jest.mock('../CountryListHeader')
+jest.mock('../CountryListLegend')
+
+beforeEach(() => {
+  jest.clearAllMocks()
+})
 
 describe('components/countryList/CountryListHeaders', () => {
   test('It renders', () => {
     render(
-      <FieldsContextProvider>
-        <CountryListHeaders />
-      </FieldsContextProvider>
+      <CountryListHeaders 
+        hiddenFields={[]}
+        sortBy="country"
+        sortAsc="true"
+      />
     )
     expect(Wrapper).toHaveBeenCalledTimes(2)
     expect(CountryListHeader).toHaveBeenCalledTimes(4)
     expect(CountryListLegend).toHaveBeenCalledTimes(4)
   })
+
+  test('It renders correctly with population hidden', () => {
+    render(
+      <CountryListHeaders 
+        hiddenFields={['population']}
+        sortBy="country"
+        sortAsc="true"
+      />
+    )
+    expect(CountryListHeader).toHaveBeenCalledTimes(3)
+    expect(CountryListLegend).toHaveBeenCalledTimes(3)
+  })
+
+  test('It renders correctly with everything hidden', () => {
+    render(
+      <CountryListHeaders 
+        hiddenFields={['population', 'area', 'density']}
+        sortBy="country"
+        sortAsc="true"
+      />
+    )
+    expect(CountryListHeader).toHaveBeenCalledTimes(1)
+    expect(CountryListLegend).toHaveBeenCalledTimes(1)
+  })
+
 })
