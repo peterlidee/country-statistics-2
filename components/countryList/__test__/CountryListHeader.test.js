df8df52c10149c12b6ed4c09edcb1199069b750b
peterlidee <peter@lidee.be>
2022-09-26 16:50:07 +0200

    Updated test for changes made

--- components/countryList/__test__/CountryListHeader.test.js
+++ components/countryList/__test__/CountryListHeader.test.js
@@ -6,26 +6,72 @@ import Wrapper from '../../general/Wrapper'
 import IconSort from '../../svgSnippets/IconSort'
 // import FieldsContext
 
+jest.mock('next/router', () => ({
+  useRouter: jest.fn(() => ({ query: {} }))
+}))
 jest.mock('../../general/Wrapper', () => {
   return jest.fn((props) => <>{props.children}</>)
 })
 jest.mock('../../svgSnippets/IconSort')
 
+beforeEach(() => {
+  jest.clearAllMocks()
+})
+
+function setup(sortBy, sortAsc){
+  render(
+    <CountryListHeader 
+      field={{ 
+        slug: 'country',
+        label: 'label',
+        sortAscDefault: true,
+      }} 
+      sortBy={sortBy}
+      sortAsc={sortAsc}
+    />
+  )
+  const linkEl = screen.getByRole('link')
+  return {
+    linkEl
+  }
+}
+
 describe('components/countryList/CountryListHeader', () => {
-  test('It return nothing when no field.display', () => {
-    render(<CountryListHeader field={{}} handleSort={() => {}} />)
-    expect(Wrapper).not.toHaveBeenCalled()
-    expect(screen.queryByRole('button')).not.toBeInTheDocument()
-  })
+
   test('It renders', () => {
-    render(<CountryListHeader field={{ 
-      display: true,
-      field: 'test',
-      label: 'label',
-      sortActive: true,
-      sortAsc: true,
-    }} handleSort={() => {}} />)
+    const { linkEl } = setup('foobar', true)
     expect(Wrapper).toHaveBeenCalled()
+    expect(linkEl).toHaveTextContent('label')
+    expect(IconSort).toHaveBeenCalled()
+  })
+
+  test('It renders with sortBy empty and sort Asc true', () => {
+    const { linkEl } = setup('foobar', true)
+    expect(linkEl).toHaveAttribute('href', '/?sort=country')
+    expect(IconSort).toHaveBeenCalledWith(
+      expect.objectContaining({
+        sortActive: false,
+        sortAsc: true,
+      }),
+      expect.anything()
+    )
+  })
+
+  test('It renders with sortBy empty and sort Asc false', () => {
+    const { linkEl } = setup('foobar', false)
+    expect(linkEl).toHaveAttribute('href', '/?sort=country')
+    expect(IconSort).toHaveBeenCalledWith(
+      expect.objectContaining({
+        sortActive: false,
+        sortAsc: false,
+      }),
+      expect.anything()
+    )
+  })
+
+  test('It renders with sortBy country and sort Asc true', () => {
+    const { linkEl } = setup('country', true)
+    expect(linkEl).toHaveAttribute('href', '/?sort=-country')
     expect(IconSort).toHaveBeenCalledWith(
       expect.objectContaining({
         sortActive: true,
@@ -33,9 +79,17 @@ describe('components/countryList/CountryListHeader', () => {
       }),
       expect.anything()
     )
-    const button = screen.getByRole('button')
-    expect(button).toBeInTheDocument()
-    expect(button).toHaveTextContent('label')
-    expect(button).toHaveClass('button__sort--test')
   })
+
+  test('It renders with sortBy country and sort Asc false', () => {
+    const { linkEl } = setup('country', false)
+    expect(IconSort).toHaveBeenCalledWith(
+      expect.objectContaining({
+        sortActive: true,
+        sortAsc: false,
+      }),
+      expect.anything()
+    )
+  })
+
 })
