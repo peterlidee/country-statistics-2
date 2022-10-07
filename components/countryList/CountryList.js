464c08ce876c0cfec92b70e18d31c9b6b102fc25
peterlidee <peter@lidee.be>
2022-10-07 15:38:46 +0200

    Updated countrylist + test

--- components/countryList/CountryList.js
+++ components/countryList/CountryList.js
@@ -1,27 +1,19 @@
-import { useContext, useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
-import PropTypes from 'prop-types';
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
+import PropTypes from 'prop-types'
 
-import fieldsData from '../fields/fieldsData';
-
-import FieldsContext from '../context/FieldsContext';
-import NumberFiltersContext from '../context/NumberFiltersContext';
-import RegionFilterContext from '../context/RegionFilterContext';
+import filterCountriesByRegion from '../../lib/regionFilter/filterCountriesByRegion'
+import applyNumberFilters from '../../lib/numberFilter/applyNumberFilters'
+import applySorting from '../../lib/sorting/applySorting'
 
 import CountryCount from '../header/CountryCount';
 import Filters from '../filters/Filters';
 import CountryListHeaders from './CountryListHeaders';
 import CountryRow from './CountryRow';
 
-import sortCountries from '../../lib/sortCountries';
-import filterCountriesByRegion from '../../lib/filterCountriesByRegion';
-import filterCountriesByNumbers from '../../lib/filterCountriesByNumbers';
-
 function CountryList({ countries, filterData }){
   
-  const { fields } = useContext(FieldsContext);
   const router = useRouter()
-
   // since we are using SSG, the query object on the router will be empty, it needs to be hydrated first
   // https://nextjs.org/docs/advanced-features/automatic-static-optimization#how-it-works
   // but, this leads to the list of countries being default ordered first and then after hydration getting sorted
@@ -34,80 +26,13 @@ function CountryList({ countries, filterData }){
     }
   }, [router.isReady])
 
+  // 1. check the display options
 
-  const { getActiveRegionFilters } = useContext(RegionFilterContext);
-  const { areaSelection, populationSelection, densitySelection, numberFilterIsActive } = useContext(NumberFiltersContext);
-
-
-  // TODO: get sort query from router and pass it to countryrow?
-
-  // 1. check the filter options
-
-  // 1.A.
-  // first do the region filters 
-  // check if there are active regionFilters or not
-  const activeRegions = getActiveRegionFilters();
-  // run filter by region
-  const countriesFilteredByRegion = filterCountriesByRegion(countries, filterData.regionIndexes, activeRegions);
-
-  // 1.B.
-  // then do the number filters
-  // from fields find out if the filter is displayed or not
-  // if it is, check if the filter is active
-  const activeNumberFilters = [];
-  for( let i = 0; i < fields.length; i++){
-    // is the filter displayed?
-    if(fields[i].display && fields[i].field !== "country"){ // country is not a filter
-      if(numberFilterIsActive(fields[i].field)){ // is the filter active?
-        activeNumberFilters.push(fields[i].field)
-      }
-    }
-  }
-  // for each value in activeNumberFilters we know it's displayed and active (a selection has been made)
-  // now do the actual filtering
-  const countriesFilteredByNumbers = filterCountriesByNumbers(countriesFilteredByRegion, activeNumberFilters, areaSelection, populationSelection, densitySelection);
-  
-  
-  // 2. sorting
-  // we need to (1) sort the countries and (2) save the router.query.sort so we can pass it to countryRow
-  
-  // copy countries data in case !routerReady
-  let sortedCountries = countriesFilteredByNumbers
-  // init default sortAsc and sortBy
-  let sortBy = fieldsData[0].slug
-  let sortAsc = fieldsData[0].sortAscDefault
-
-  if(routerReady){
-    
-    // default sorting
-    let currFieldIndex = 0
-
-    // is there a sorting param and is it valid
-    if(router.query.hasOwnProperty('sort')){
-      sortAsc = !router.query.sort.startsWith('-')
-      // get the param out of the query
-      const passedSortBy = sortAsc ? router.query.sort : router.query.sort.slice(1)
-      // is the param valid? we check it against fieldsData
-      currFieldIndex = fieldsData.findIndex(field => field.slug === passedSortBy)
-      // if passedSortBy is valid, if was found in fieldsData and index > -1
-      // else we pass 0 for the default sort
-      if(currFieldIndex > -1) sortBy = passedSortBy
-    }
-    
-    // sort the countries
-    sortedCountries = sortCountries(
-      [...countriesFilteredByNumbers], 
-      fieldsData[currFieldIndex].sortKey, 
-      sortAsc, 
-      fieldsData[currFieldIndex].sortType
-    )
-  }
-
-
-  // 3. check the display options
-  // we don't wait for routerReady because the headers are only display when routerReady
-  // 3.1 we need to know how many fields are displayed to set inline grid
-  // 3.2 pass hiddenFields to CountryListHeaders and countryRow
+  // we don't wait for routerReady because the headers are only displayed when routerReady
+  // we need to know display fields in 3 cases
+  // - to filter (we don't filter hidden fields)
+  // - to set grid
+  // - to pass to CountryListHeaders and countryRow
   
   // get the val for hide out of query
   const hiddenFieldsQuery = router.query?.hide
@@ -119,28 +44,45 @@ function CountryList({ countries, filterData }){
   const gridTemplateColumnsStyle = numberOfVisibleFields < 1 ? 
     { "gridTemplateColumns": "1.5em minmax(9em, 15em)"} : 
     { "gridTemplateColumns": `1.5em minmax(9em, 15em) repeat(${numberOfVisibleFields}, minmax(auto, 9em))`};
-  
-    
-  // 4. display data
-  /*  TODO check count!!!!! */
+
+  // 2. check the filter options
+
+  // 2.A. apply region filters 
+  // check if there are active regionFilters or not
+  const activeRegions = router.query.hasOwnProperty('regions') && router.query.regions !== '' ? router.query.regions.split(',') : []
+  // run filter by region
+  const countriesFilteredByRegion = filterCountriesByRegion(countries, filterData.regionIndexes, activeRegions);
+
+  // 2.B. apply number filters
+  const countriesFilteredByNumbers = applyNumberFilters(hiddenFields, router.query, filterData, countriesFilteredByRegion)
+
+  // 3. sorting
+  const { countries: sortedCountries, sortBy, sortAsc } = applySorting(routerReady, router.query, countriesFilteredByNumbers)
+
   return(
     <div className="site__grid--home">
-      <CountryCount count={sortedCountries.length} />
-      <Filters filterData={filterData} />
-      <main className="country-list" style={gridTemplateColumnsStyle}>
-        {routerReady && 
+      {!routerReady && '...loading'}
+      {routerReady && <>
+        <CountryCount count={sortedCountries.length} />
+        <Filters 
+          hiddenFields={hiddenFields}
+          filterData={filterData} />
+        <main className="country-list" style={gridTemplateColumnsStyle}>
           <CountryListHeaders 
             hiddenFields={hiddenFields} 
             sortBy={sortBy} 
             sortAsc={sortAsc} />
-        }
-        {routerReady && sortedCountries.map((country, i) => 
-          <CountryRow 
-            country={country} 
-            index={i} 
-            key={country.cca3} />
-        )}
-      </main>
+          {sortedCountries.length === 0 && 
+            <div style={{ 'gridColumn': '2/-1' }}>no results</div>
+          }
+          {sortedCountries.map((country, i) => 
+            <CountryRow 
+              country={country} 
+              index={i} 
+              key={country.cca3} 
+              hiddenFields={hiddenFields} />)}
+        </main>
+      </>}
     </div>
   )
 }
--- components/countryList/__test__/CountryList.test.js
+++ components/countryList/__test__/CountryList.test.js
@@ -1,9 +1,9 @@
 import { screen, render } from '@testing-library/react'
-import {toBeInTheDocument } from '@testing-library/jest-dom'
+import { toBeInTheDocument } from '@testing-library/jest-dom'
 
-import { FieldsContextProvider } from '../../context/FieldsContext'
-import { NumberFiltersContextProvider } from '../../context/NumberFiltersContext'
-import { RegionFilterContextProvider } from '../../context/RegionFilterContext'
+import { useRouter } from 'next/router'
+// todo 
+import { RegionFilterContextProvider2 } from '../../context/RegionFilterContext2'
 
 import CountryList from '../CountryList'
 import CountryCount from '../../header/CountryCount'
@@ -11,37 +11,163 @@ import Filters from '../../filters/Filters'
 import CountryListHeaders from '../CountryListHeaders'
 import CountryRow from '../CountryRow'
 
-import countriesMock from '../../../__mock__/data/countriesMock'
+import { extraDataCountries } from '../../../__mock__/data/countriesMock'
 import filterDataMock from '../../../__mock__/data/filterDataMock'
 
-const { extraDataCountries } = countriesMock
-
+jest.mock('next/router', () => ({
+  useRouter: jest.fn()
+}))
 jest.mock('../../header/CountryCount')
 jest.mock('../../filters/Filters')
 jest.mock('../CountryListHeaders')
 jest.mock('../CountryRow')
 
+beforeEach(() => {
+  jest.clearAllMocks()
+})
+
+function setupRender(){
+  const { container } = render(
+    <RegionFilterContextProvider2 filterData={filterDataMock}>
+      <CountryList countries={extraDataCountries} filterData={filterDataMock} />
+    </RegionFilterContextProvider2>
+  )
+  return { container }
+}
+function setupReturnValue(query, ready = true){
+  useRouter.mockReturnValue({
+    isReady: ready,
+    query: query,
+  })
+}
+
 describe('components/countryList/CountryList', () => {
+  
   test('It renders', () => {
-    const { container } = render(
-      <FieldsContextProvider>
-        <RegionFilterContextProvider defaultRegionState={filterDataMock.defaultRegionState}>
-          <NumberFiltersContextProvider filterData={filterDataMock}>
-            <CountryList countries={extraDataCountries} filterData={filterDataMock} />
-          </NumberFiltersContextProvider>
-        </RegionFilterContextProvider>
-      </FieldsContextProvider>
-    )
+    setupReturnValue({})
+    const { container } = setupRender()
 
     expect(container.querySelector('.site__grid--home')).toBeInTheDocument()
-    expect(CountryCount).toHaveBeenCalledWith(
-      { count: 6 },
-      expect.anything()
-    )
+    expect(CountryCount).toHaveBeenCalled()
     expect(Filters).toHaveBeenCalled()
+    expect(container.querySelector('.country-list')).toBeInTheDocument()
     expect(CountryListHeaders).toHaveBeenCalled()
     expect(CountryRow).toHaveBeenCalledTimes(6)
+  })
+  
+  test('It renders loading', () => {
+    setupReturnValue({}, false)
+    setupRender()
+    expect(screen.getByText('...loading')).toBeInTheDocument()
+  })
+
+  test('Grid has the correct styles with none hidden', () => {
+    setupReturnValue({})
+    const { container } = setupRender()
+    expect(container.querySelector('.country-list')).toHaveStyle('gridTemplateColumns: 1.5em minmax(9em, 15em) repeat(3, minmax(auto, 9em))')
+  })
+
+  test('Grid has the correct styles with 1 hidden', () => {
+    setupReturnValue({ hide: 'area' })
+    const { container } = setupRender()
+    expect(container.querySelector('.country-list')).toHaveStyle('gridTemplateColumns: 1.5em minmax(9em, 15em) repeat(2, minmax(auto, 9em))')
+  })
+
+  test('Grid has the correct styles with all hidden', () => {
+    setupReturnValue({ hide: 'area,population,density' })
+    const { container } = setupRender()
+    expect(container.querySelector('.country-list')).toHaveStyle('gridTemplateColumns: 1.5em minmax(9em, 15em)')
+  })
+
+  test('It displays no results', () => {
+    setupReturnValue({ area: '1,2' })    
+    setupRender()
+    expect(screen.getByText(/no results/i)).toBeInTheDocument()
+  })
+
+  test('CountryCount mock was called with correct props when all countries are displayed', () => {
+    setupReturnValue({})
+    setupRender()
+    expect(CountryCount).toHaveBeenCalledWith(
+      expect.objectContaining({
+        count: 6
+      }),
+      expect.anything()
+    )
+  })
+
+  test('CountryCount mock was called with correct props when filters active', () => {
+    setupReturnValue({ area: '0,50000' })
+    setupRender()
+    expect(CountryCount).toHaveBeenLastCalledWith(
+      expect.objectContaining({
+        count: 4
+      }),
+      expect.anything()
+    )
+  })
+
+  test('Filters was called with the correct hiddenFields props when nothing hidden', () => {
+    setupReturnValue({})
+    setupRender()
+    expect(Filters).toHaveBeenLastCalledWith(
+      expect.objectContaining({
+        hiddenFields: []
+      }),
+      expect.anything()
+    )
+  })
+
+  test('Filters was called with the correct hiddenFields props when area hidden', () => {
+    setupReturnValue({ hide: 'area' })
+    setupRender()
+    expect(Filters).toHaveBeenLastCalledWith(
+      expect.objectContaining({
+        hiddenFields: [ 'area' ]
+      }),
+      expect.anything()
+    )
+  })
+
+  test('CountryListHeaders gets called with the correct props when default', () => {
+    setupReturnValue({})
+    setupRender()
+    expect(CountryListHeaders).toHaveBeenLastCalledWith(
+      expect.objectContaining({
+        sortBy: 'country',
+        sortAsc: true
+      }),
+      expect.anything()
+    )
+  })
+
+  test('CountryListHeaders gets called with the correct props when sort -density', () => {
+    setupReturnValue({ sort: '-density' })
+    setupRender()
+    expect(CountryListHeaders).toHaveBeenLastCalledWith(
+      expect.objectContaining({
+        sortBy: 'density',
+        sortAsc: false,
+      }),
+      expect.anything()
+    )
+  })
+
+  test('CountryRow is called with correctly when default filter', () => {
+    setupReturnValue({})
+    setupRender()
     expect(CountryRow.mock.calls[0][0].country.cca3).toBe('AUT')
     expect(CountryRow.mock.calls[5][0].country.cca3).toBe('SGS')
   })
+
+  test('CountryRow is called with correctly when default filter', () => {
+    setupReturnValue({ population: '0,10000000' })
+    setupRender()
+    expect(CountryRow).toHaveBeenCalledTimes(4)
+    expect(CountryRow.mock.calls[0][0].country.cca3).toBe('AUT')
+    expect(CountryRow.mock.calls[1][0].country.cca3).toBe('DNK')
+    expect(CountryRow.mock.calls[2][0].country.cca3).toBe('PRI')
+    expect(CountryRow.mock.calls[3][0].country.cca3).toBe('SGS')
+  })
+
 })
