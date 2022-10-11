46927ed78c18aff78f3e898efac87cd8fba72be9
peterlidee <peter@lidee.be>
2022-10-11 18:18:41 +0200

    updated and cleaned up test

--- components/general/__test__/Collapse.test.js
+++ components/general/__test__/Collapse.test.js
@@ -1,57 +1,70 @@
 import { screen, render } from '@testing-library/react'
-import { toBeInTheDocument } from '@testing-library/jest-dom'
 import userEvent from '@testing-library/user-event'
 
 import Collapse from '../Collapse'
 import Wrapper from '../Wrapper'
 
-const elements = {}
-
-beforeEach(() => {
-  const { container } = render(<Collapse label="label" extraClass="" />)
-  elements.button = screen.getByRole('button')
-  elements.content = container.querySelector('.collapse__content')
-  elements.buttonLabelEl = elements.button.querySelector('.collapse__label')
-  elements.buttonStatusEl = elements.button.querySelector('.collapse__status')
-})
-
 jest.mock('../Wrapper', () => {
   return jest.fn((props) => <>{props.children}</>)
 })
+const ChildMock = jest.fn()
 
-describe('components/general/Collapse renders', () => {
-  test('It renders the wrapper mock and its children', () => {
+const setupRender = () => {
+  const { container } = render(
+    <Collapse label="label" extraClass="extraClass">
+      <ChildMock />
+    </Collapse>
+  )
+  const button = screen.getByRole('button')
+  // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+  const content = container.querySelector('.collapse__content')
+  return { button, content }
+}
+
+describe('components/general/Collapse', () => {
+
+  test('It renders', () => {
+    const { button, content } = setupRender()
+
     expect(Wrapper).toHaveBeenCalled()
+    expect(button).toBeInTheDocument()
+    expect(button).toHaveTextContent(/label/i)
+    expect(button).toHaveTextContent(/\+/)
+    expect(content).toBeInTheDocument()
+    expect(ChildMock).toHaveBeenCalled()
   })
-  test('It renders content container and button', () => {
-    expect(elements.content).toBeInTheDocument()
-    expect(elements.button).toBeInTheDocument()
-  })
-  test('The button has the correct label', () => {
-    expect(elements.buttonLabelEl).toHaveTextContent('label')
-  })
-})
 
-describe('components/general/Collapse works correctly', () => {
-  test('It has the correct initial values', () => {
-    expect(elements.buttonStatusEl).toHaveTextContent('+')
-    expect(elements.content).toHaveStyle('display: none')
-  })
-  test('It toggles', async () => {
-    const user = userEvent.setup()
-    await user.click(elements.button)
-    expect(elements.buttonStatusEl).toHaveTextContent('-')
-    expect(elements.content).toHaveStyle('display: block')
-  })
-  test('It toggles many times', async () => {
-    const user = userEvent.setup()
-    await user.click(elements.button)
-    await user.click(elements.button)
-    await user.click(elements.button)
-    expect(elements.buttonStatusEl).toHaveTextContent('-')
-    expect(elements.content).toHaveStyle('display: block')
-    await user.click(elements.button)
-    expect(elements.buttonStatusEl).toHaveTextContent('+')
-    expect(elements.content).toHaveStyle('display: none')
+  describe('It collapses', () => {
+
+    test('It has the correct initial values', () => {
+      const { button, content } = setupRender()
+
+      expect(button).toHaveTextContent(/\+/)
+      expect(content).toHaveStyle('display: none')
+    })
+
+    test('It toggles', async () => {
+      const { button, content } = setupRender()
+      const user = userEvent.setup()
+
+      await user.click(button)
+      expect(button).toHaveTextContent(/\-/)
+      expect(content).toHaveStyle('display: block')
+    })
+
+    test('It toggles many times', async () => {
+      const { button, content } = setupRender()
+      const user = userEvent.setup()
+
+      await user.click(button)
+      await user.click(button)
+      await user.click(button)
+      expect(button).toHaveTextContent(/\-/)
+      expect(content).toHaveStyle('display: block')
+      await user.click(button)
+      expect(button).toHaveTextContent(/\+/)
+      expect(content).toHaveStyle('display: none')
+    })
+
   })
 })
