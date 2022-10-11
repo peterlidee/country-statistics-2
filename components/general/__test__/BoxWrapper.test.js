b75b642b9a1a777f83ba7f032950575181522be7
peterlidee <peter@lidee.be>
2022-10-11 18:02:39 +0200

    Updated and cleaned up test

--- components/general/__test__/BoxWrapper.test.js
+++ components/general/__test__/BoxWrapper.test.js
@@ -1,23 +1,19 @@
-import { screen, render } from '@testing-library/react'
-import { toBeInTheDocument } from '@testing-library/jest-dom'
+import { render } from '@testing-library/react'
 
 import BoxWrapper from '../BoxWrapper'
-
 const ChildMock = jest.fn()
 
 describe('components/general/BoxWrapper', () => {
+
   test('It renders', () => {
     const { container } = render(<BoxWrapper name="test" />)
+    // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
     expect(container.querySelector('.single-country__test')).toBeInTheDocument()
   })
-  test('It prints the correct class', () => {
-    const { container, rerender } = render(<BoxWrapper name="test" />)
-    expect(container.querySelector('.single-country__test')).toBeInTheDocument()
-    rerender(<BoxWrapper name="test2" />)
-    expect(container.querySelector('.single-country__test2')).toBeInTheDocument()
-  })
+
   test('It passes children correctly', () => {
     render(<BoxWrapper name="test"><ChildMock /></BoxWrapper>)
     expect(ChildMock).toHaveBeenCalled()
   })
+  
 })
