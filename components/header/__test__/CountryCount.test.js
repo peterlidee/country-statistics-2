201c4f8459bac3c678b26c0134e84ec1a48c7738
peterlidee <peter@lidee.be>
2022-10-12 11:49:26 +0200

    Updated and cleaned test

--- components/header/__test__/CountryCount.test.js
+++ components/header/__test__/CountryCount.test.js
@@ -1,23 +1,29 @@
-import { render, screen, toHaveBeenCalled } from '@testing-library/react'
-import { toBeInTheDocument } from '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
 
 import CountryCount from '../CountryCount'
 
 describe('components/header/CountryCount', () => {
+
   test('It renders', () => {
-    const { container } = render(<CountryCount count={100} />)
-    expect(container.querySelector('.country-count')).toBeInTheDocument()
+    render(<CountryCount count={100} />)
+    expect(screen.getByText(/displaying/i)).toBeInTheDocument()
+    expect(screen.getByText('100')).toBeInTheDocument()
+    expect(screen.getByText(/countries/i)).toBeInTheDocument()
   })
-  test('It displays the correct count', () => {
-    const { container, rerender } = render(<CountryCount count={100} />)
-    const number = container.querySelector('.country-count__number')
-    expect(number).toHaveTextContent('100')
-    expect(screen.queryByText(/countries/i)).toBeInTheDocument()
-    rerender(<CountryCount count={10} />)
-    expect(number).toHaveTextContent('10')
-    expect(screen.queryByText(/countries/i)).toBeInTheDocument()
-    rerender(<CountryCount count={1} />)
-    expect(number).toHaveTextContent('1')
-    expect(screen.queryByText(/country/i)).toBeInTheDocument()
+
+  test('It displays the correct count 100', () => {
+    render(<CountryCount count={100} />)
+    expect(screen.getByText('100')).toBeInTheDocument()
   })
+
+  test('It displays the correct count 10', () => {
+    render(<CountryCount count={10} />)
+    expect(screen.getByText('10')).toBeInTheDocument()
+  })
+
+  test('It displays the correct count 1', () => {
+    render(<CountryCount count={1} />)
+    expect(screen.getByText('1')).toBeInTheDocument()
+  })
+
 })
