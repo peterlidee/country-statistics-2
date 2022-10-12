43daa13bd7566086fd62021e41c239d3c3253bde
peterlidee <peter@lidee.be>
2022-10-12 20:04:53 +0200

    Updated test

--- components/single/__test__/BreadCrumb.test.js
+++ components/single/__test__/BreadCrumb.test.js
@@ -1,13 +1,14 @@
 import { screen, render } from '@testing-library/react'
-import { toBeInTheDocument } from '@testing-library/jest-dom'
 
 import BreadCrumb from '../BreadCrumb'
 
 describe('components/single/BreadCrumb', () => {
+
   test('It renders', () => {
-    const { container } = render(<BreadCrumb countryName="Algeria" />)
-    expect(container.querySelector('.breadcrumb')).toBeInTheDocument()
-    expect(container.querySelector('.breadcrumb__home')).toBeInTheDocument()
+    render(<BreadCrumb countryName="Algeria" />)
+    expect(screen.getByRole('link', { name: /all countries/i })).toBeInTheDocument()
+    expect(screen.getByText('>')).toBeInTheDocument()
     expect(screen.getByText('Algeria')).toBeInTheDocument()
   })
+  
 })
