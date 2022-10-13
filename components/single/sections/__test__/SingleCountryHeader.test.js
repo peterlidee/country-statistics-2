d3d67e6b5a4483c0420e402f0fb32b909b3230a9
peterlidee <peter@lidee.be>
2022-10-13 17:26:03 +0200

    Updated tets

--- components/single/sections/__test__/SingleCountryHeader.test.js
+++ components/single/sections/__test__/SingleCountryHeader.test.js
@@ -1,19 +1,18 @@
 import { screen, render } from '@testing-library/react'
-import { toBeInTheDocument } from '@testing-library/jest-dom'
-
 import SingleCountryHeader from '../SingleCountryHeader'
 
 const ChildMock = jest.fn()
 
 describe('components/single/sections/SingleCountryHeader', () => {
+
   test('It renders', () => {
-    const { container } = render(
+    render(
       <SingleCountryHeader countryName='Algeria'>
         <ChildMock />
       </SingleCountryHeader>
     )
-    expect(container.querySelector('.single-country__header')).toBeInTheDocument()
     expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent('Algeria')
     expect(ChildMock).toHaveBeenCalled()
   })
+
 })
