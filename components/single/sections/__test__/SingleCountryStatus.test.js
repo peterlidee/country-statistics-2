0ff49c7234b631855b3d5986a70545dd0aef83d2
peterlidee <peter@lidee.be>
2022-10-13 17:35:15 +0200

    Updated test

--- components/single/sections/__test__/SingleCountryStatus.test.js
+++ components/single/sections/__test__/SingleCountryStatus.test.js
@@ -1,13 +1,12 @@
 import { screen, render } from '@testing-library/react'
-import { toBeInTheDocument } from '@testing-library/jest-dom'
-
 import SingleCountryStatus from '../SingleCountryStatus'
 
 const ChildMock = jest.fn()
 
 describe('components/single/sections/SingleCountryStatus', () => {
+
   test('It renders with error', () => {
-    const { container } = render(
+    render(
       <SingleCountryStatus 
         loading={false} 
         error={new Error('An error')} 
@@ -16,14 +15,12 @@ describe('components/single/sections/SingleCountryStatus', () => {
         <ChildMock />
       </SingleCountryStatus>
     )
-    expect(container.querySelector('.single-country__status')).toBeInTheDocument()
-    expect(container.querySelector('.error-message')).toBeInTheDocument()
     expect(screen.getByText('No data found for aaa')).toBeInTheDocument()
     expect(ChildMock).toHaveBeenCalled()
   })
 
   test('It renders with no data and no error', () => {
-    const { container } = render(
+    render(
       <SingleCountryStatus 
         loading={false} 
         error={undefined} 
@@ -32,14 +29,12 @@ describe('components/single/sections/SingleCountryStatus', () => {
         <ChildMock />
       </SingleCountryStatus>
     )
-    expect(container.querySelector('.single-country__status')).toBeInTheDocument()
-    expect(container.querySelector('.error-message')).toBeInTheDocument()
     expect(screen.getByText('No data found for aaa')).toBeInTheDocument()
     expect(ChildMock).toHaveBeenCalled()
   })
 
   test('It renders with data and no error', () => {
-    const { container } = render(
+    render(
       <SingleCountryStatus 
         loading={false} 
         error={undefined} 
@@ -48,8 +43,8 @@ describe('components/single/sections/SingleCountryStatus', () => {
         <ChildMock />
       </SingleCountryStatus>
     )
-    expect(container.querySelector('.single-country__status')).toBeInTheDocument()
-    expect(container.querySelector('.error-message')).not.toBeInTheDocument()
+    expect(screen.queryByText(/no data found for/i)).not.toBeInTheDocument()
     expect(ChildMock).toHaveBeenCalled()
   })
+
 })
