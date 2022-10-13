1116f9f8d664e473fa78756890441c771d2d19ac
peterlidee <peter@lidee.be>
2022-10-13 17:30:56 +0200

    Updated test

--- components/single/sections/__test__/SingleCountryRegion.test.js
+++ components/single/sections/__test__/SingleCountryRegion.test.js
@@ -1,5 +1,4 @@
 import { screen, render } from '@testing-library/react'
-import { toBeInTheDocument } from '@testing-library/jest-dom'
 
 import singleCountryMocks from '../../../../__mock__/data/singleCountryMocks'
 import SingleCountryRegion from '../SingleCountryRegion'
@@ -12,15 +11,13 @@ jest.mock('../../neighbours/ValidateNeighbouringCountries')
 describe('components/single/sections/SingleCountryRegion', () => {
   
   test('It renders with data', () => {
-    const { container } = render(
+    render(
       <SingleCountryRegion 
         loading={false}
         error={undefined}
         data={singleCountryMocks[0]}
       />
     )
-    expect(container.querySelector('.single-country__region')).toBeInTheDocument()
-    expect(container.querySelector('.single-country__box')).toBeInTheDocument()
     expect(RenderLabelValue.mock.calls).toHaveLength(3)
     expect(RenderLabelValue.mock.calls[0][0]).toMatchObject({
       loading: false,
@@ -50,7 +47,6 @@ describe('components/single/sections/SingleCountryRegion', () => {
   })
 
   test('It renders with data but no region, subregion or capital', () => {
-    jest.resetAllMocks()
     render(
       <SingleCountryRegion 
         loading={false}
@@ -78,8 +74,7 @@ describe('components/single/sections/SingleCountryRegion', () => {
   })
 
   test('It rerenders correctly', () => {
-    jest.clearAllMocks()
-    const { container, rerender } = render(
+    const { rerender } = render(
       <SingleCountryRegion 
         loading={false}
         error={undefined}
